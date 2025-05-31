40e7ffe91a4192e9c3b2905eaea034befb47eb76
Priyatham Dasigandla <dasigandla.priyatham@gmail.com>
2025-05-30 17:32:11 -0700

    Step 3: Player Win Logic Developed

---
+++ src/Day 7 - Hangman/Day 7 - Hangman.py
@@ -0,0 +1,32 @@
+import random
+
+word_list = ["aardvark", "baboon", "camel", "donkey", "elephant", "flamingo", "giraffe", "hippopotamus", "iguana", "jaguar"]
+
+chosen_word_text = random.choice(word_list)
+print(f"Chosen word: {chosen_word_text}")  # For debugging purposes
+chosen_word = list(chosen_word_text.lower())
+placeholder = ''
+
+for i in range(len(chosen_word)):
+    placeholder += '_'
+print(placeholder)
+
+placeholder_list = list(placeholder)
+guess_list = []
+
+while '_' in placeholder_list:
+    guess = input("Guess a letter: ").lower()
+    if guess in guess_list:
+        print("You already guessed that letter. Try again.")
+        continue
+    else:
+        guess_list.append(guess)
+
+    for i in range(len(chosen_word)):
+        if chosen_word[i] == guess:
+            placeholder_list[i] = guess
+
+    updated_placeholder = ''.join(placeholder_list)
+    print(updated_placeholder)
+
+print(f"Congratulations! You've guessed the word: {updated_placeholder}")
