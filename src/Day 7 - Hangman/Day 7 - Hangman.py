d5aebb55b93f64f7bacc8d673eab51535a7a3822
Priyatham Dasigandla <dasigandla.priyatham@gmail.com>
2025-06-03 01:18:14 -0700

    Step 4: Lives, Logic, and ASCII!!

--- src/Day 7 - Hangman/Day 7 - Hangman.py
+++ src/Day 7 - Hangman/Day 7 - Hangman.py
@@ -1,6 +1,6 @@
 import random
 
-stages = ['''
+stages = [r'''
   +---+
   |   |
   O   |
@@ -8,7 +8,7 @@ stages = ['''
  / \  |
       |
 =========
-''', '''
+''', r'''
   +---+
   |   |
   O   |
@@ -16,7 +16,7 @@ stages = ['''
  /    |
       |
 =========
-''', '''
+''', r'''
   +---+
   |   |
   O   |
@@ -24,14 +24,14 @@ stages = ['''
       |
       |
 =========
-''', '''
+''', r'''
   +---+
   |   |
   O   |
  /|   |
       |
       |
-=========''', '''
+=========''', r'''
   +---+
   |   |
   O   |
@@ -39,7 +39,7 @@ stages = ['''
       |
       |
 =========
-''', '''
+''', r'''
   +---+
   |   |
   O   |
@@ -47,7 +47,7 @@ stages = ['''
       |
       |
 =========
-''', '''
+''', r'''
   +---+
   |   |
       |
@@ -59,10 +59,12 @@ stages = ['''
 
 word_list = ["aardvark", "baboon", "camel", "donkey", "elephant", "flamingo", "giraffe", "hippopotamus", "iguana", "jaguar"]
 
-chosen_word_text = random.choice(word_list)
+# chosen_word_text = random.choice(word_list)
+chosen_word_text = "aardvark"  # For testing purposes
 print(f"Chosen word: {chosen_word_text}")  # For debugging purposes
 chosen_word = list(chosen_word_text.lower())
 placeholder = ''
+lives = 6
 
 for i in range(len(chosen_word)):
     placeholder += '_'
@@ -72,9 +74,10 @@ placeholder_list = list(placeholder)
 guess_list = []
 
 while '_' in placeholder_list:
+    correctGuess = False
     guess = input("Guess a letter: ").lower()
     if guess in guess_list:
-        print("You already guessed that letter. Try again.")
+        print("You already guessed that letter. Try again, maybe with a different letter this time.")
         continue
     else:
         guess_list.append(guess)
@@ -82,8 +85,21 @@ while '_' in placeholder_list:
     for i in range(len(chosen_word)):
         if chosen_word[i] == guess:
             placeholder_list[i] = guess
+            correctGuess = True
 
+    if correctGuess == False:
+        print("Incorrect guess.")
+        lives -= 1
+
+    print(stages[lives])
+
+    if lives == 0:
+        print("You lost! The word was:", chosen_word_text)
+        exit()      
+          
     updated_placeholder = ''.join(placeholder_list)
-    print(updated_placeholder)
+
+    if '_' in updated_placeholder:
+        print(f"Current Word: {updated_placeholder}.\nYou have {lives} lives remaining.\nKeep Trying\n" )
 
 print(f"Congratulations! You've guessed the word: {updated_placeholder}")
