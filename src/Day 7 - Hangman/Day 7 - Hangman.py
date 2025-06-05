7dd897e80e04bb6ceeb3913225a07e1e5576b671
Priyatham Dasigandla <dasigandla.priyatham@gmail.com>
2025-06-04 20:06:00 -0700

    Finished working on game thru logic and better function handling

--- src/Day 7 - Hangman/Day 7 - Hangman.py
+++ src/Day 7 - Hangman/Day 7 - Hangman.py
@@ -2,48 +2,54 @@ import random
 import hangman_words
 import hangman_art
 
-chosen_word_text = random.choice(hangman_words.word_list)
-print(f"Chosen word: {chosen_word_text}")  # For debugging purposes
-
-chosen_word = list(chosen_word_text.lower())
-placeholder = ''
-lives = 6
-
-for i in range(len(chosen_word)):
-    placeholder += '_'
-print(f"Word to guess: {placeholder}")
-
-placeholder_list = list(placeholder)
-gameOver = False
-guess_list = []
-
-while not gameOver:
+def proceedIfNewChar(wordOfDeath, wordOfLife, guessOfGame, lifeOfHangman):
     correctGuess = False
-    guess = input("Guess a letter: ").lower()
-    if guess in guess_list:
-        print("You already guessed that letter. Try again, maybe with a different letter this time.")
-        continue
-    else:
-        guess_list.append(guess)
-
-    for i in range(len(chosen_word)):
-        if chosen_word[i] == guess:
-            placeholder_list[i] = guess
+    for i in range(len(wordOfDeath)):
+        if wordOfDeath[i] == guessOfGame:
+            wordOfLife[i] = guessOfGame
             correctGuess = True
 
-    if correctGuess == False:
-        print("Incorrect guess.")
-        lives -= 1
+    if correctGuess == True:
+        print("That letter is indeed in the game. Good guess!")
+    else:
+        print("Yikes! That letter ain't in the word. Try again. In the meantime, you lose a life.")
+        lifeOfHangman -= 1
+    
+    return wordOfLife, lifeOfHangman
 
+lives = 6
+guess_list = []
+placeholder = []
+
+print("Welcome to" + hangman_art.logo)
+
+chosen_word_text = random.choice(hangman_words.word_list)
+chosen_word = list(chosen_word_text.lower())
+
+for i in range(len(chosen_word)):
+    placeholder.append('_')
+
+while '_' in placeholder:
+    print(f"Word to guess: {''.join(placeholder)}")
+
+    #guess = input("Guess a letter: ").lower()
+    while True:
+        guess = input("Guess a letter: ").lower()
+        if len(guess) == 1 and guess.isalpha():
+            break
+        else:
+            print("Please enter a single alphabetical character.")
+
+    if guess in guess_list:
+        print("You've already guessed that letter. Try again.")
+    else:
+        guess_list.append(guess)
+        placeholder, lives = proceedIfNewChar(chosen_word, placeholder, guess, lives)
+
+    print(f"You have {lives}/6 lives remaining.")
     print(hangman_art.stages[lives])
-
     if lives == 0:
-        print("You lost! The word was:", chosen_word_text)
-        exit()      
-          
-    updated_placeholder = ''.join(placeholder_list)
+        print(f"You lost! The word was: {chosen_word_text}")
+        exit()
 
-    if '_' in updated_placeholder:
-        print(f"Current Word: {updated_placeholder}.\nYou have {lives} lives remaining.\nKeep Trying\n" )
-
-print(f"Congratulations! You've guessed the word: {updated_placeholder}")
+print(f"Congratulations! You've guessed the word: {''.join(placeholder)}")
