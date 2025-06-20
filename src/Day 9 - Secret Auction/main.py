2a64c567522e1261240c0403d59a2536682153c0
Priyatham Dasigandla <dasigandla.priyatham@gmail.com>
2025-06-19 19:57:25 -0700

    Implemented better terminal functionality for a more smoother experience

--- src/Day 9 - Secret Auction/main.py
+++ src/Day 9 - Secret Auction/main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from art import logo
 
 bidTable = {}
@@ -7,7 +8,25 @@ highestBidder = ""
 highestBid = 0
 
 def clear():
+    # nt is for Windows, posix is for Unix/Linux/Mac
     os.system('cls' if os.name == 'nt' else 'clear')
+
+# Function to wait for a key press without echoing it to the console
+def keyPress():
+    try:
+        #For Windows
+        if sys.platform.startswith('win'):
+            import msvcrt
+            msvcrt.getch()
+            
+        #For Unix/Linux/Mac
+        else:
+            import tty, termios
+            tty.setraw(sys.stdin.fileno())
+            sys.stdin.read(1)
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, termios.tcgetattr(sys.stdin))
+    except Exception as e:
+        input("Something went wrong. Press any key to exit.")
 
 def nextBid():
     continueBid = input("Are there any other bidders? Type 'yes' or 'no': ")
@@ -19,14 +38,20 @@ def nextBid():
         print("Please start using your brain.")
         return nextBid()
 
-print("Welcome to the Secret Auction Program!")
-print(logo)
-input("\nPress any key to continue...")
+clear()
+print(f"Welcome to the Secret Auction Program!{logo}\n Press any key to start bidding.")
+keyPress()
 
 while not biddingFinish:
     clear()
     name = input("Name for this bid: ")
-    bid = float(input("Bid Amount: $"))
+    while True:
+        try:
+            bid = float(input("Bid Amount: $"))
+            break
+        except ValueError:
+            print("Please enter a valid number for the bid amount.")
+
     bidTable[name] = bid
 
     biddingFinish = nextBid()
@@ -36,4 +61,5 @@ for bidder in bidTable:
         highestBid = bidTable[bidder]
         highestBidder = bidder
 
+clear()
 print(f"The highest bidder is {highestBidder} with a bid of ${highestBid:.2f}.")
